e08b8e0570f7410fd1d027c30d9184fb4d29e3ca
ryoogh96 <ryoogh96@gmail.com>
2023-08-21 19:04:58 +0900

    feat: final implementation LockFreeQueue

--- ServerEngine/Utility/LockFreeQueue.hpp
+++ ServerEngine/Utility/LockFreeQueue.hpp
@@ -5,60 +5,178 @@ namespace Engine
 	template<typename T>
 	class LockFreeQueue
 	{
+		struct Node;
+
+		struct CountedNodePtr
+		{
+			int32 externalCount; // acquire right to refer
+			Node* ptr = nullptr;
+		};
+
+		struct NodeCounter
+		{
+			uint32 internalCount : 30; // return acquire right to refer
+			uint32 externalCountRemaining : 2; // acquire multiple right to refer Push & Pop
+		};
+
 		struct Node
 		{
-			std::shared_ptr<T> data;
-			Node* next = nullptr;
+			Node()
+			{
+				NodeCounter newCount;
+				newCount.internalCount = 0;
+				newCount.externalCountRemaining = 2;
+				count.store(newCount);
+
+				next.ptr = nullptr;
+				next.externalCount = 0;
+			}
+
+			void ReleaseRef()
+			{
+				NodeCounter oldCounter = count.load();
+
+				while (true)
+				{
+					NodeCounter newCounter = oldCounter;
+					newCounter.internalCount--;
+
+					if (count.compare_exchange_strong(oldCounter, newCounter))
+					{
+						if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+							delete this;
+
+						break;
+					}
+				}
+			}
+
+			std::atomic<T*> data;
+			std::atomic<NodeCounter> count;
+			CountedNodePtr next;
 		};
 
 	public:
-		LockFreeQueue() : m_Head(new Node), m_Tail(m_Head)
+		LockFreeQueue()
 		{
+			CountedNodePtr node;
+			node.ptr = new Node;
+			node.externalCount = 1;
 
+			m_Head.store(node);
+			m_Tail.store(node);
 		}
 
 		LockFreeQueue(const LockFreeQueue&) = delete;
 		LockFreeQueue& operator=(const LockFreeQueue&) = delete;
 
+		// [data][]
+		// [head][tail]
 		void Push(const T& value)
 		{
-			std::shared_ptr<T> newData = std::make_shared<T>(value);
+			std::unique_ptr<T> newData = std::make_unique<T>(value);
 
-			Node* dummy = new Node();
+			CountedNodePtr dummy;
+			dummy.ptr = new Node;
+			dummy.externalCount = 1;
 
-			Node* oldTail = m_Tail;
-			oldTail->data.swap(newData);
-			oldTail->next = dummy;
+			CountedNodePtr oldTail = m_Tail.load(); // ptr = nullptr
 
-			m_Tail = dummy;
+			while (true)
+			{
+				// the thread that increase externalCount by 1 right now, acquire right to refer
+				IncreaseExternalCount(m_Tail, oldTail);
+
+				// thet thread that exchange data first, acquire right to own
+				T* oldData = nullptr;
+				if (oldTail.ptr->data.compare_exchange_strong(oldData, newData.get()))
+				{
+					oldTail.ptr->next = dummy;
+					oldTail = m_Tail.exchange(dummy);
+					FreeExternalCount(oldTail);
+					newData.release(); // give up the data of unique_ptr<T>
+					break;
+				}
+
+				// the thread that fail to acquire right to own
+				oldTail.ptr->ReleaseRef();
+			}
 		}
 
 		std::shared_ptr<T> TryPop()
 		{
-			Node* oldHead = PopHead();
-			if (oldHead == nullptr)
-				return std::shared_ptr<T>();
+			// [data][data][ ]
+			// [head][tail]
 
-			std::shared_ptr<T> res(oldHead->data);
-			delete oldHead;
-			return res;
+			CountedNodePtr oldHead = m_Head.load();
+
+			while (true)
+			{
+				// the thread that increase externalCount by 1 right now, acquire right to refer
+				IncreaseExternalCount(m_Head, oldHead);
+
+				Node* ptr = oldHead.ptr;
+				if (ptr == m_Tail.load().ptr)
+				{
+					ptr->ReleaseRef();
+					return std::shared_ptr<T>();
+				}
+
+				// acquire right to won (head = ptr->next)
+				if (m_Head.compare_exchange_strong(oldHead, ptr->next))
+				{
+					T* res = ptr->data.load(); // there is bug when using exchange(nullptr);
+					FreeExternalCount(oldHead);
+					return std::shared_ptr<T>(res);
+				}
+
+				ptr->ReleaseRef();
+			}
 		}
 
 	private:
-		Node* PopHead()
+		static void IncreaseExternalCount(std::atomic<CountedNodePtr>& counter, CountedNodePtr& oldCounter)
 		{
-			Node* oldHead = m_Head;
-			if (oldHead == m_Tail)
-				return nullptr;
+			while (true)
+			{
+				CountedNodePtr newCounter = oldCounter;
+				newCounter.externalCount++;
 
-			m_Head = oldHead->next;
-			return oldHead;
+				if (counter.compare_exchange_strong(oldCounter, newCounter))
+				{
+					oldCounter.externalCount = newCounter.externalCount;
+					break;
+				}
+			}
+		}
+
+		static void FreeExternalCount(CountedNodePtr& oldNodePtr)
+		{
+			Node* ptr = oldNodePtr.ptr;
+			const int32 countIncrease = oldNodePtr.externalCount - 2;
+
+			NodeCounter oldCounter = ptr->count.load();
+
+			while (true)
+			{
+				NodeCounter newCounter = oldCounter;
+				newCounter.externalCountRemaining--;
+				newCounter.internalCount += countIncrease;
+
+				if (ptr->count.compare_exchange_strong(oldCounter, newCounter))
+				{
+					if (newCounter.internalCount == 0 && newCounter.externalCountRemaining == 0)
+						delete ptr;
+
+					break;
+				}
+			}
 		}
 
 	private:
 		// [data][data][]
 		// [head][tail]
-		Node* m_Head = nullptr;
-		Node* m_Tail = nullptr;
+		std::atomic<CountedNodePtr> m_Head;
+		std::atomic<CountedNodePtr> m_Tail;
 	};
 }
